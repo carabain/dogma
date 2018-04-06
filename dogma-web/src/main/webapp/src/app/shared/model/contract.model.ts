d94e25a6c9e9546e64da96eef6ca1778b5f11129
jasperbosch <jasperbosch@chello.nl>
2018-04-06 20:18:01 +0200

    Einde van dag 1

---
+++ dogma-web/src/main/webapp/src/app/shared/model/contract.model.ts
@@ -0,0 +1,9 @@
+export class Contract {
+
+  host: string;
+  netwerkid: string;
+  smartContractAddress: string;
+  smartContractAbi: string;
+  smartContractBin: string;
+  oracleids: string[] = [];
+}
--- dogma-web/src/main/webapp/src/app/shared/model/oracle.model.ts
+++ dogma-web/src/main/webapp/src/app/shared/model/oracle.model.ts
@@ -1,5 +1,5 @@
 export class Oracle {
-  id: number;
+  id: string;
   name: string;
   description: string;
   creationDate: any;
