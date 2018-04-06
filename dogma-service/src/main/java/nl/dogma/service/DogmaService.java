1ee45fcad42f7a316ff43cc8d738fcd74688e10c
Bert Gritter <bertgritter@gmail.com>
2018-04-06 18:16:06 +0200

    Restservice aangemaakt maar hij doet het nog niet

--- dogma-service/src/main/java/nl/dogma/service/DogmaService.java
+++ dogma-service/src/main/java/nl/dogma/service/DogmaService.java
@@ -1,6 +1,9 @@
 package nl.dogma.service;
 
+import nl.dogma.domain.oracle.domain.DutchBrOracle;
+
 public interface DogmaService {
 
 
+    DutchBrOracle getDutchBrOracle();
 }
--- dogma-service/src/main/java/nl/dogma/service/DogmaServiceImpl.java
+++ dogma-service/src/main/java/nl/dogma/service/DogmaServiceImpl.java
@@ -1,5 +1,22 @@
 package nl.dogma.service;
 
-public class DogmaServiceImpl implements DogmaService {
+
+import nl.dogma.domain.oracle.domain.DutchBrOracle;
+import nl.dogma.domain.oracle.domain.DutchBrOracleFactory;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class DogmaServiceImpl
+        implements DogmaService {
+
+
+    @Override
+    @Path("/getOracles")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DutchBrOracle getDutchBrOracle() {
+        return DutchBrOracleFactory.getInstance().getDutchBrOracle();
+    }
 
 }
